e3ba64ec8da0e913c7ba036b1fbcbc282cd1c7cc
Emeldalaeticia <114027865+Emeldalaeticia@users.noreply.github.com>
2023-06-20 23:04:41 +0300

    Updated the authenication system

--- users/views.py
+++ users/views.py
@@ -11,27 +11,30 @@ from django.contrib.auth.views import LoginView
 from django.contrib.auth.forms import UserCreationForm
 
 from django.urls import reverse_lazy
+from .forms import SignUpForm
 
 class SignUpView(CreateView):
 
     template_name = 'users/signup.html'
 
-    form_class = UserCreationForm
+    form_class = SignUpForm
 
     success_url = reverse_lazy('photo:list')
 
     def form_valid(self, form):
         to_return = super().form_valid(form)
 
-        user = authenticate(
-            username=form.cleaned_data["username"],
-            password=form.cleaned_data["password1"],
-        )
+        username = form.cleaned_data["username"]
+        password = form.cleaned_data["password1"]
+        email = form.cleaned_data["email"]
+
+        user = User.objects.create_user(username=username, email=email, password=password)
+        user = authenticate(username=username, password=password)
 
         login(self.request, user)
 
         return to_return
 
 class CustomLoginView(LoginView):
-
     template_name = 'users/login.html'
+    redirect_authenticated_user = True
